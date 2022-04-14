a238700f3705cea3fadbada2a806d6f598148ef4
jmalucchi <71409486+jmalucchi@users.noreply.github.com>
2022-04-13 18:45:50 -0700

    Update settings.py

--- logo_detect_api/settings.py
+++ logo_detect_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g9c84sobcqjqggou9nvcin!$0^q9ohh6&-l65n5$pow+@^qfmq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*"]
 
 
 # Application definition
